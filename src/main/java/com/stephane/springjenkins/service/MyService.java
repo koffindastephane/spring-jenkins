e7bd20408ee09fa910adea743aecc01127776bca
Stephane Koffi <koffindastephane@gmail.com>
2023-03-06 17:28:28 +0000

    Eighth commit.

--- src/main/java/com/stephane/springjenkins/service/MyService.java
+++ src/main/java/com/stephane/springjenkins/service/MyService.java
@@ -1,11 +1,14 @@
 package com.stephane.springjenkins.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class MyService {
 
     public String greet(String greeting) {
+        log.info("Executing greet service";
         return greeting;
     }
 
